f95f85f003156325f2f64d50c0c15048def09459
Saisurya Kattamuri <saisurya.kat@ascendcorp.com>
2018-08-07 14:15:40 +0700

    Add example with Person

--- src/main/java/exec/Main.java
+++ src/main/java/exec/Main.java
@@ -1,10 +1,17 @@
 package exec;
 
-import io.saisuryak.lab.objectmappings.model.domainobject.AddressDO;
-import objectmappings.mapper.AddressMapperKt;
+import io.saisuryak.lab.objectmappings.model.datatransferobject.PersonDTO;
+import io.saisuryak.lab.objectmappings.model.domainobject.*;
+import objectmappings.mapper.PersonMapperKt;
+
+import java.time.Instant;
+import java.util.List;
+
+import static java.util.Arrays.asList;
 
 public class Main {
     public static void main(String[] args) {
+        //================================================
         AddressDO addressDO = new AddressDO();
         addressDO.setHouseNumber("1A");
         addressDO.setAddressLine1("Street Quechua");
@@ -12,7 +19,49 @@ public class Main {
         addressDO.setState("Corvus");
         addressDO.setCountry("Peaceful");
         addressDO.setZipCode("1234567");
-        String address = AddressMapperKt.mapAddressDOToAddressDTO(addressDO);
-        System.out.println(address);
+        //=================================================
+        ContactInfoDO contactDO1 = new ContactInfoDO();
+        contactDO1.setType(ContactTypeDO.EMAIL);
+        contactDO1.setValue("abc@xyz.com");
+        contactDO1.setActive(true);
+        ContactInfoDO contactDO2 = new ContactInfoDO();
+        contactDO2.setType(ContactTypeDO.FIXEDLINB);
+        contactDO2.setValue("+1 0923423434");
+        contactDO2.setActive(false);
+        List<ContactInfoDO> contactsDO = asList(contactDO1, contactDO2);
+        //=================================================
+        RelationDO relationDO1 = new RelationDO();
+        relationDO1.setName("XYZ");
+        relationDO1.setType(RelationTypeDO.BROTHER);
+        RelationDO relationDO2 = new RelationDO();
+        relationDO2.setName("WUV");
+        relationDO2.setType(RelationTypeDO.SISTER);
+        List<RelationDO> relationsDO = asList(relationDO1, relationDO2);
+        //=================================================
+        PersonDO personDO = new PersonDO();
+        personDO.setId(1L);
+        personDO.setFirstName("Corvus");
+        personDO.setMiddleName("aura");
+        personDO.setLastName("Signo");
+        personDO.setAge("24");
+        personDO.setGenderDO(GenderDO.MALE);
+        personDO.setDob(Instant.EPOCH);
+        personDO.setFatherName("ESport");
+        personDO.setMotherName("Logi");
+        personDO.setContacts(contactsDO);
+        personDO.setAddressDO(addressDO);
+        personDO.setHobbies(asList("Reading", "Writing"));
+        personDO.setIdentifierNumber("1234567890");
+        personDO.setNationalityDO(NationalityDO.ENGLISH);
+        QualificationDO qualificationDO1 = new QualificationDO();
+        qualificationDO1.setName("ABC");
+        qualificationDO1.setType(QualificationTypeDO.SCHOOLING);
+        qualificationDO1.setFromYear("2000");
+        qualificationDO1.setToYear("2010");
+        personDO.setQualificationsDO(asList(qualificationDO1));
+        personDO.setRelationsDO(relationsDO);
+        PersonDTO personDTO = PersonMapperKt.mapPersonDOToPersonDTO(personDO);
+        System.out.println(personDTO);
+
     }
 }
