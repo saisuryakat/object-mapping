5b3e24a600d2437ea2e6a74f43f039001c60a626
Saisurya Kattamuri <saisurya.kat@ascendcorp.com>
2018-08-07 13:39:21 +0700

    Simplify AddressMapper with kotlin with and add @JvmStatic for Java access

--- src/main/kotlin/objectmappings/mapper/AddressMapperKt.kt
+++ src/main/kotlin/objectmappings/mapper/AddressMapperKt.kt
@@ -3,15 +3,8 @@ package objectmappings.mapper
 import io.saisuryak.lab.objectmappings.model.domainobject.AddressDO
 
 object AddressMapperKt {
-
+    @JvmStatic
     fun mapAddressDOToAddressDTO(addressDO: AddressDO): String {
-        return """
-            ${addressDO.houseNumber}
-            ${addressDO.addressLine1}
-            ${addressDO.addressLine2}
-            ${addressDO.state}
-            ${addressDO.country}
-            ${addressDO.zipCode}
-            """
+        return with(addressDO) { "$houseNumber $addressLine1 $addressLine2 $state $country $zipCode" }
     }
 }
