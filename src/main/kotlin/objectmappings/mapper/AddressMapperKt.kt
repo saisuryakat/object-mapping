3fb2cf60e46adf0a4f2d93e43b80f2223071689f
Saisurya Kattamuri <saisurya.kat@ascendcorp.com>
2018-08-24 15:40:17 +0700

    Use Kotlin Elvis operator for handling null strings in template

--- src/main/kotlin/objectmappings/mapper/AddressMapperKt.kt
+++ src/main/kotlin/objectmappings/mapper/AddressMapperKt.kt
@@ -5,6 +5,6 @@ import io.saisuryak.lab.objectmappings.model.domainobject.AddressDO
 object AddressMapperKt {
     @JvmStatic
     fun mapAddressDOToAddressDTO(addressDO: AddressDO): String {
-        return with(addressDO) { "$houseNumber $addressLine1 $addressLine2 $state $country $zipCode" }
+        return with(addressDO) { "$houseNumber ${addressLine1 ?: ""} ${addressLine2 ?: ""} $state $country $zipCode" }
     }
 }
